4a20feb8d21a9de11fb743541fcee67174dde8f1
Yeo Kheng Meng <yeokm1@gmail.com>
2016-09-24 09:53:07 +0800

    arduino code to receive temperature and humidity data

--- hardware/arduino-temp-humidity/arduino-temp-humidity.ino
+++ hardware/arduino-temp-humidity/arduino-temp-humidity.ino
@@ -1,31 +1,39 @@
 #include <DHT.h>
 
-#define DHTPIN 11     // what digital pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define PIN_DHT 11
+#define DHTTYPE DHT22
 
-DHT dht(DHTPIN, DHTTYPE);
+#define PIN_ONBOARD_LED 13
+
+DHT dht(PIN_DHT, DHTTYPE);
 
 void setup(){
-
   Serial.begin(9600);
   Serial1.begin(9600);
-  sendToBothSerialPrintln("Sensor start");
+
+  pinMode(PIN_ONBOARD_LED, OUTPUT);
+  delay(500); //Wait for serial port to initialise
 
   dht.begin();
+
+  //Turn on LED momentarily to show end of setup function
+  digitalWrite(PIN_ONBOARD_LED, HIGH);
+  delay(1000);
+  digitalWrite(PIN_ONBOARD_LED, LOW);
 }
 
 
 void loop(){
-
-  delay(2000);
-
+  
+  delay(2000); //DHT22 requires 2 seconds to measure a reading
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
   String result = generateResultString(temperature, humidity);
 
+  digitalWrite(PIN_ONBOARD_LED, HIGH);
   sendToBothSerialPrintln(result);
-  
+  digitalWrite(PIN_ONBOARD_LED, LOW);
 }
 
 String generateResultString(float temperature, float humidity){

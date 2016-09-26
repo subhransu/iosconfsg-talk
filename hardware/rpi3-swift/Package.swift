74b3e7a1cb10c2b8e984ca421b3ead41aa75b9d6
Yeo Kheng Meng <yeokm1@gmail.com>
2016-09-26 12:51:05 +0000

    change package name

--- hardware/rpi3-swift/Package.swift
+++ hardware/rpi3-swift/Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "bbb-swift",
+    name: "rpi3-swift",
     dependencies: [
     	.Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0),
     ]

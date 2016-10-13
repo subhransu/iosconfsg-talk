e50fa6f4fd7ff8815d943e5578983a1a566eaca8
Yeo Kheng Meng <yeokm1@gmail.com>
2016-10-13 23:18:19 +0800

    Update Package.swift

--- hardware/rpi3-swift/Package.swift
+++ hardware/rpi3-swift/Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "rpi3-swift",
     dependencies: [
     	.Package(url: "https://github.com/uraimo/SwiftyGPIO.git", "0.8.7"),
-    	.Package(url: "https://github.com/yeokm1/SwiftLinuxSerial.git", "0.0.3"),
+    	.Package(url: "https://github.com/yeokm1/SwiftLinuxSerial.git", "0.0.5"),
     ]
 )
